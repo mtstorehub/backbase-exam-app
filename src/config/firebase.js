2e578570c5d0ef105fad6b6be879382e923b996c
MT <myat.thu@yomabank.com>
2019-07-29 11:29:00 +0630

    drop firebase api key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 import Firebase from 'firebase'
- let config = {
-    apiKey: "AIzaSyC7Cth1fPdXc5CnGGloOXpGL7dKe3MpFQQ",
-    authDomain: "backbase-exam-app.firebaseapp.com",
-    databaseURL: "https://backbase-exam-app.firebaseio.com",
-    projectId: "backbase-exam-app",
-    storageBucket: "backbase-exam-app.appspot.com",
-    messagingSenderId: "1036545621854",
-    appId: "1:1036545621854:web:74d8da8fd0a73f24"
-  };
+let config = {
+  apiKey: "xxxxx",
+  authDomain: "xxxxx",
+  databaseURL: "xxxxx",
+  projectId: "xxxxx",
+  storageBucket: "xxxxx",
+  messagingSenderId: "xxxxx",
+  appId: "xxxxx"
+};
 let app = Firebase.initializeApp(config)
 export const database = app.database()
